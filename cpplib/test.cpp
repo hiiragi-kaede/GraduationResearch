27410fcd7f7e972e2397864680c88dc3993f6213
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-11-05 12:01:01 +0900

    pybind11のおためし継続

--- cpplib/test.cpp
+++ cpplib/test.cpp
@@ -1,5 +1,8 @@
+#include<iostream>
 #include<vector>
+#include<string>
 #include<pybind11/pybind11.h>
+#include <pybind11/embed.h>
 #include<pybind11/stl.h>
 
 namespace py = pybind11;
@@ -21,9 +24,19 @@ vector<vector<int>> get_mat(const int size){
     return mat;
 }
 
+void print_data(const vector<vector<int>> dis_mat){
+    for(size_t i=0; i<dis_mat.size(); i++){
+        for(size_t j=0; j<dis_mat[0].size(); j++){
+            std::cout<<dis_mat[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
 PYBIND11_MODULE(test, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
     m.def("get_mat",&get_mat,"Get 2 dimention vector");
+    m.def("print_data",&print_data,"print data");
 }
