3b60a314be59c691dd2aec58cfcbe022619dc12a
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-12-01 11:27:03 +0900

    近傍リストのサイズ変更しての比較実験

--- VRP/cpp/out/probe.py
+++ VRP/cpp/out/probe.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-fname="n228_two_simple.txt"
-data1,data2=[],[]
+fname="n228/10p_two_simple.txt"
+data1=[]
 with open(fname) as f:
     truck_size=int(f.readline())
     tmp=[[] for _ in range(truck_size)]
@@ -17,33 +17,42 @@ with open(fname) as f:
         tmp[trucks[1]].append(score_dif)
     data1=tmp
 
-fname="n228_twofast_simple.txt"
-with open(fname) as f:
-    truck_size=int(f.readline())
-    tmp=[[] for _ in range(truck_size)]
-    while True:
-        trucks=f.readline().split()
-        if(trucks[0][0]=="t"): break
-        
-        trucks=list(map(int,trucks))
-        score_dif=float(f.readline())
-        tmp[trucks[0]].append(score_dif)
-        tmp[trucks[1]].append(score_dif)
+fnames=["n228/10p_twofast_simple.txt",
+        "n228/15p_twofast.txt",
+        "n228/20p_twofast.txt"]
+types=["10% twofast","15% twofast","20% twofast"]
+datas=[]
+for fname in fnames:
+    with open(fname) as f:
+        truck_size=int(f.readline())
+        tmp=[[] for _ in range(truck_size)]
+        while True:
+            trucks=f.readline().split()
+            if(trucks[0][0]=="t"): break
+            
+            trucks=list(map(int,trucks))
+            score_dif=float(f.readline())
+            tmp[trucks[0]].append(score_dif)
+            tmp[trucks[1]].append(score_dif)
 
-    data2=tmp
-    cols=['type','truck','order','diff']
-    df=pd.DataFrame(index=[],columns=cols)
-    for i in range(len(data1)):
-        ele=data1[i]
+        datas.append(tmp)
+
+cols=['type','truck','order','diff']
+df=pd.DataFrame(index=[],columns=cols)
+for i in range(len(data1)):
+    ele=data1[i]
+    for order in range(len(ele)):
+        tmp=pd.DataFrame(data=[['two',i,order,ele[order]]],columns=cols)
+        df=df.append(tmp,ignore_index=True)
+for j in range(len(types)):
+    for i in range(len(datas[j])):
+        ele=datas[j][i]
         for order in range(len(ele)):
-            tmp=pd.DataFrame(data=[['two',i,order,ele[order]]],columns=cols)
+            tmp=pd.DataFrame(data=[[types[j],i,order,ele[order]]],columns=cols)
             df=df.append(tmp,ignore_index=True)
-    for i in range(len(data2)):
-        ele=data2[i]
-        for order in range(len(ele)):
-            tmp=pd.DataFrame(data=[['two_fast',i,order,ele[order]]],columns=cols)
-            df=df.append(tmp,ignore_index=True)
-    
-    df = df.sort_values('truck')
-    sns.boxenplot(x='truck',y='diff',hue='type',data=df)
-    plt.show()
+
+df = df.sort_values('truck')
+sns.boxenplot(x='truck',y='diff',hue='type',data=df)
+labels=['two']+types
+plt.legend(labels=labels,loc='best')
+plt.show()
