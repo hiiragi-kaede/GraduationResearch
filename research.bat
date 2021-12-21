fa75d6a6e6642b479dfb11b6f0e587f3b04a9dcb
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-12-21 19:13:37 +0900

    ファイル名を渡しただけで4回実行されるように

--- research.bat
+++ research.bat
@@ -1,4 +1,16 @@
-cd .\VRP\cpp\
+@echo off
+cd research_out
+mkdir %~n1_out 2>NUL
+cd ../VRP/cpp
 g++ -Wno-format-security -O3 -o main main.cpp src/construct.cpp src/util.cpp src/improve.cpp src/tools.cpp -pthread
-main.exe < ../CVRP_Data/tsplib/eilA101.txt > ../../research_out/eilA101_out.txt
+main.exe -n=ilsc -k=d< ../CVRP_Data/%1 > ../../research_out/%~n1_out/cross_bridge.txt 2>NUL
+main.exe -n=ilsc -k=f< ../CVRP_Data/%1 > ../../research_out/%~n1_out/cross_four.txt 2>NUL
+echo; >> ../../research_out/%~n1_out/cross_bridge.txt
+echo; >> ../../research_out/%~n1_out/cross_four.txt
+for /l %%n in (1,1,3) do (
+    main.exe -n=ilsc -k=d< ../CVRP_Data/%1 >> ../../research_out/%~n1_out/cross_bridge.txt 2>NUL
+    main.exe -n=ilsc -k=f< ../CVRP_Data/%1 >> ../../research_out/%~n1_out/cross_four.txt 2>NUL
+    echo; >> ../../research_out/%~n1_out/cross_bridge.txt
+    echo; >> ../../research_out/%~n1_out/cross_four.txt
+)
 cd ../../
--- research.py
+++ research.py
@@ -1,4 +1,6 @@
 import subprocess
 
+#CVRP_Data以下のディレクトリやファイル名を指定
+fname="vrplib/X-n856-k95.txt"
 #batファイルに入力データ名を渡してコンパイルから書き出しまでを自動で行わせる
-subprocess.run(['research.bat'])
+subprocess.run(['research.bat',fname])
