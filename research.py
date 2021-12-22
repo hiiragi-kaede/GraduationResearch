7949478dee6db97ee6b096c2786f284fb290dfa6
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-12-22 13:34:54 +0900

    複数ファイル対応を確認

--- research.py
+++ research.py
@@ -1,6 +1,8 @@
 import subprocess
 
 #CVRP_Data以下のディレクトリやファイル名を指定
-fname="vrplib/X-n856-k95.txt"
+fnames=["vrplib/X-n228-k23.txt",
+        "vrplib/X-n331-k15.txt"]
 #batファイルに入力データ名を渡してコンパイルから書き出しまでを自動で行わせる
-subprocess.run(['research.bat',fname])
+for fname in fnames:
+    res=subprocess.run(['research.bat',fname])
